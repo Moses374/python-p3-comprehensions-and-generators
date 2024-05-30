98a31f4222a852f60c4779562b7608044883b2ba
Moses <emperorskid@gmail.com>
2024-05-30 08:37:16 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    if not num_list:
+        return []
+    elif num_list[0] % 2 == 0:
+        return [num_list[0]] + return_evens(num_list[1:])
+    else:
+        return return_evens(num_list[1:])
+
     pass
 
 def make_exclamation(sentence_list):
+    if not sentence_list:
+        return []
+    else:
+        
+        return [sentence + "!" for sentence in sentence_list]
+
     pass
